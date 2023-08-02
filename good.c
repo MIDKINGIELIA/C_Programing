93e981c8e3ef97066c06eeed94946aac31d05383
MIDKINGIELIA <87158519+MIDKINGIELIA@users.noreply.github.com>
2023-08-02 22:27:23 +0900

    godd.c

--- good.c
+++ good.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+#include<string.h>
+#define PI 3.141592
+#define AI_NAME "Friday"
 int main(){
+    printf("I am %s\n",AI_NAME);
 
-    printf("hellow");
-    return 0;
+    char str1[] = "good";
+    char *ptr =str1;
+    printf("Input korean");
+    printf("%p",ptr);
+    int i = 2 * PI;
+
+
 }
