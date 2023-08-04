27ae4d4cabc7be6947db77d851f2888c498f5f3f
MIDKINGIELIA <87158519+MIDKINGIELIA@users.noreply.github.com>
2023-08-04 20:09:32 +0900

    printf 함수 종류

--- good.c
+++ good.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<string.h>
+#include<limits.h>
+
 #define PI 3.141592
 #define AI_NAME "Friday"
 int main(){
+    const float pi = 3.141592;
     printf("I am %s\n",AI_NAME);
 
     char str1[] = "good";
@@ -10,6 +13,6 @@ int main(){
     printf("Input korean");
     printf("%p",ptr);
     int i = 2 * PI;
-
+ 
 
 }
--- test.c
+++ test.c
@@ -3,11 +3,14 @@
 int main(void){
   char fruit[40];
 
-  printf("what is your favorte fruit\n");
+  // printf("what is your favorte fruit\n");
 
-  scanf("%s",fruit);
-  printf("you lick %s\n",fruit);
+  // scanf("%s",fruit);
+  // printf("you lick %s\n",fruit);
 
-  printf("%d",14);
+  // printf("%d\n",14);
+  // printf("%+d",10);
+  printf("%*d",10,20); //은근 유용함. ->
+  printf("")
 
 }
